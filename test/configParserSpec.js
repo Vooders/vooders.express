94c979decd00152484f20bb7b7cc15a9e21f2165
Kev Wilson <kev.wilson84@gmail.com>
2018-06-24 12:41:16 +0100

    should throw config not found error if path is invalid

--- test/configParserSpec.js
+++ test/configParserSpec.js
@@ -1,16 +1,18 @@
 const Bluebird = require('bluebird')
 const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 const Gen = require('verify-it').Gen
 const fs = require('fs')
 
 const ConfigParser = require('../src/ConfigParser')
 
+chai.use(chaiAsPromised)
 chai.should()
 
 describe('ConfigParser', () => {
   const genPath = () => `test/generatedFiles/${Gen.word()}.json`
 
-  const genConfigFile = (path, config) => {
+  const writeConfigFile = (path, config) => {
     const content = JSON.stringify(config)
     fs.writeFileSync(path, content)
   }
@@ -26,11 +28,17 @@ describe('ConfigParser', () => {
     const path = genPath()
 
     return Bluebird.resolve()
-      .then(() => genConfigFile(path, config))
+      .then(() => writeConfigFile(path, config))
       .then(() => parser.load(path))
       .then((result) => result.should.eql(config))
       .finally(() => {
         removeConfigFile(path)
       })
+  })
+
+  verify.it('should throw config not found error if path is invalid', () => {
+    const parser = new ConfigParser()
+    const path = genPath()
+    parser.load(path).should.eventually.throw(Error('invalid config path'))
   })
 })
